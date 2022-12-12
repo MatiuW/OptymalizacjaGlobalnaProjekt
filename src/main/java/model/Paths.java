c028832a953eb0b3375370903ec0a4462acfb3e0
kuba8705 <62181332+kuba8705@users.noreply.github.com>
2022-12-12 22:30:55 +0100

    Add files via upload

--- src/main/java/model/Paths.java
+++ src/main/java/model/Paths.java
@@ -3,6 +3,9 @@ package model;
 import java.util.Random;
 import java.util.List;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Paths {
 
     Path paths[];
@@ -28,19 +31,27 @@ public class Paths {
     
     private class Path
     {
-        private Travel pathTravel[];
+        private List<Travel> pathTravel;
         private int lengthOfPath;
 
         Path(List<Travel> travels)
         {
-            Random rand = new Random();
-            lengthOfPath = rand.nextInt(5) + 2;
 
-            pathTravel = new Travel[lengthOfPath];
+            Random rand = new Random();
+            lengthOfPath = 6;
+
+            pathTravel = new ArrayList();
 
             rand = new Random();
+
             int lastTravel = rand.nextInt(travels.size());
-            pathTravel[0] = travels.get(lastTravel);
+            pathTravel.add(travels.get(lastTravel));
+
+            for(int i = 0; i < travels.size(); i++)
+            {
+                if(travels.get(lastTravel).getCityfirst().equals(travels.get(i).getCityfirst()) && travels.get(lastTravel).getCitysecond().equals(travels.get(i).getCitysecond())) travels.remove(i);
+                if(travels.get(lastTravel).getCityfirst().equals(travels.get(i).getCitysecond()) && travels.get(lastTravel).getCitysecond().equals(travels.get(i).getCityfirst())) travels.remove(i);
+            }
 
             String lastCitysecond = travels.get(lastTravel).getCitysecond();
             int currentTravel;
@@ -53,15 +64,23 @@ public class Paths {
                 {
                     currentTravel = rand.nextInt(travels.size());
                     
-                    if(lastCitysecond.equals(travels.get(currentTravel).getCityfirst()))
-                    {
-                        //System.out.println(lastCitysecond + " " + travels.get(currentTravel).getCityfirst());
-                        break;
-                    }
+                    if(lastCitysecond.equals(travels.get(currentTravel).getCityfirst())) break;
                 }
 
                 lastCitysecond = travels.get(currentTravel).getCitysecond();
-                pathTravel[i] = travels.get(currentTravel);
+                pathTravel.add(travels.get(currentTravel));
+
+                for(int q = 0; i < travels.size(); q++)
+                {
+                    if(travels.get(currentTravel).getCityfirst().equals(travels.get(q).getCityfirst()) && travels.get(currentTravel).getCitysecond().equals(travels.get(q).getCitysecond()))
+                    {
+                        travels.remove(q);
+                    }
+                    if(travels.get(currentTravel).getCityfirst().equals(travels.get(q).getCitysecond()) && travels.get(currentTravel).getCitysecond().equals(travels.get(q).getCityfirst()))
+                    {
+                        travels.remove(q);
+                    }
+                }
             }
         }
         public void write_out()
@@ -73,8 +92,8 @@ public class Paths {
 
             for(int i = 0; i < lengthOfPath; i++)
             {
-                System.out.println(pathTravel[i].getCityfirst() + " =====> " + pathTravel[i].getCitysecond()+" Odleglosc: " + pathTravel[i].getLength());
-                l = l + Float.parseFloat(pathTravel[i].getLength());
+                System.out.println(pathTravel.get(i).getCityfirst() + " =====> " + pathTravel.get(i).getCitysecond()+" Odleglosc: " + pathTravel.get(i).getLength());
+                l = l + Float.parseFloat(pathTravel.get(i).getLength());
             }
             System.out.println();
             System.out.println("Długość ścieżki: " + l);
